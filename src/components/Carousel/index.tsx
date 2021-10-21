b81a7f9594e501d918b4df7d02a3203079bd7431
arpitdalal <arpitdalal@gmail.com>
2021-10-21 09:26:24 -0400

    make the Carousel component touch responsive

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,12 +1,35 @@
+import { useContext, useCallback } from "react";
 import { Grid, Typography } from "@mui/material";
+import { useSwipeable } from "react-swipeable";
+
+import { AppContext, ContextType } from "../../context/appContext";
 import NextBtn from "../NextBtn";
 import PrevBtn from "../PrevBtn";
+import { isNextMonthYear, isPrevMonthYear } from "../../utils";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Carousel = ({ children }: Props) => {
+  const { setPrevMonthYear, setNextMonthYear, monthYears, selectedMonthYear } =
+    useContext(AppContext) as ContextType;
+
+  const swipePrevMontYear = useCallback(() => {
+    isPrevMonthYear(monthYears, selectedMonthYear) === true &&
+      setPrevMonthYear();
+  }, [monthYears, selectedMonthYear, setPrevMonthYear]);
+
+  const swipeNextMontYear = useCallback(() => {
+    isNextMonthYear(monthYears, selectedMonthYear) === true &&
+      setNextMonthYear();
+  }, [monthYears, selectedMonthYear, setNextMonthYear]);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: swipeNextMontYear,
+    onSwipedRight: swipePrevMontYear,
+  });
+
   return (
     <Grid
       container
@@ -17,7 +40,7 @@ const Carousel = ({ children }: Props) => {
       <Grid item textAlign='left'>
         <PrevBtn />
       </Grid>
-      <Grid item textAlign='center'>
+      <Grid item textAlign='center' flexGrow={1} {...handlers}>
         <Typography variant='h6' component='div'>
           {children}
         </Typography>
