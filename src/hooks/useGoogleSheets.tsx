76547149fa1b55eb0edd0fef3179df7b067c220b
arpitdalal <arpitdalal@gmail.com>
2021-10-22 17:51:43 -0400

    add useGoogleSheets custom hook

---
+++ src/hooks/useGoogleSheets.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import useGoogleSheets from "use-google-sheets";
+import { ErrorResponse, Sheet } from "use-google-sheets/dist/types";
+
+const useGoogleSheetsFunc = () => {
+  const [googleData, setGoogleData] = useState<Sheet[] | null>(null);
+  const [googleLoading, setGoogleLoading] = useState<boolean>(true);
+  const [googleError, setGoogleError] = useState<ErrorResponse | null>(null);
+
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY as string;
+  const sheetId = process.env.REACT_APP_GOOGLE_SHEETS_ID as string;
+
+  const { data, loading, error } = useGoogleSheets({
+    apiKey,
+    sheetId,
+  });
+
+  useEffect(() => {
+    setGoogleData(data);
+    setGoogleLoading(loading);
+    setGoogleError(error);
+  }, [data, loading, error]);
+
+  return { googleData, googleLoading, googleError };
+};
+
+export default useGoogleSheetsFunc;
