60ac8cf72f389b6e4e18ef92a6b177969a0a777e
arpitdalal <arpitdalal@gmail.com>
2021-10-26 10:28:27 -0400

    add some margin top to the h1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,13 @@ import { Toolbar, Typography } from "@mui/material";
 const Header = () => {
   return (
     <Toolbar>
-      <Typography width='100%' textAlign='center' variant='h3' component='h1'>
+      <Typography
+        marginTop='1.5rem'
+        width='100%'
+        textAlign='center'
+        variant='h3'
+        component='h1'
+      >
         ExTrack
       </Typography>
     </Toolbar>
