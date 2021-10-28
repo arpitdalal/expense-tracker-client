914a505a54c7a03657f1edce07519826f67123db
arpitdalal <arpitdalal@gmail.com>
2021-10-28 14:45:30 -0400

    change the styles to fit text better on mobile screens

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
-import { SwipeableDrawer, Container, Box, Typography } from "@mui/material";
-import { Global } from "@emotion/react";
+import { SwipeableDrawer, Box, Typography } from "@mui/material";
 
 import DrawerForm from "../DrawerForm";
 import Puller from "../Puller";
@@ -11,64 +10,38 @@ const Drawer = () => {
     AppContext
   ) as AppContextType;
 
-  let drawerBleeding = 25;
-  process.env.REACT_APP_DRAWER_BLEEDING &&
-    (drawerBleeding = parseInt(process.env.REACT_APP_DRAWER_BLEEDING));
-
   return (
-    <>
-      <Global
-        styles={{
-          ".MuiDrawer-root > .MuiPaper-root": {
-            height: `calc(75% - ${drawerBleeding}px)`,
-            overflow: "visible",
-          },
-        }}
-      />
-      <SwipeableDrawer
-        anchor={"bottom"}
-        open={drawerOpen}
-        onClose={() => {
-          toggleDrawer(false);
-        }}
-        onOpen={() => {
-          toggleDrawer(true);
-        }}
-        PaperProps={{
-          sx: {
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-          },
-        }}
+    <SwipeableDrawer
+      anchor={"bottom"}
+      open={drawerOpen}
+      onClose={() => {
+        toggleDrawer(false);
+      }}
+      onOpen={() => {
+        toggleDrawer(true);
+      }}
+      PaperProps={{
+        sx: {
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+          py: "2rem",
+        },
+      }}
+    >
+      <Puller />
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        flexDirection='column'
+        height='100%'
       >
-        <Box
-          sx={{
-            position: "absolute",
-            top: -drawerBleeding,
-            visibility: "visible",
-            right: 0,
-            left: 0,
-            height: "100%",
-          }}
-        >
-          <Container maxWidth='xl' sx={{ height: "100%" }}>
-            <Puller />
-            <Box
-              display='flex'
-              justifyContent='center'
-              alignItems='center'
-              flexDirection='column'
-              height='100%'
-            >
-              <Typography variant='h4' textAlign='center'>
-                {expenseData.title}
-              </Typography>
-              <DrawerForm />
-            </Box>
-          </Container>
-        </Box>
-      </SwipeableDrawer>
-    </>
+        <Typography variant='h4' textAlign='center'>
+          {expenseData.title}
+        </Typography>
+        <DrawerForm />
+      </Box>
+    </SwipeableDrawer>
   );
 };
 
