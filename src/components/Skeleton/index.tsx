25c68ed5db81d9681b20c80ca9433b795fc4685d
arpitdalal <arpitdalal@gmail.com>
2021-11-09 14:13:20 -0500

    make the skeleton component work with the new style

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -1,13 +1,23 @@
 import { Skeleton } from "@mui/material";
+import { Box } from "@mui/system";
+
+import { useStyles } from "../../pages/Home";
 
 const AnimatedSkeleton = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Skeleton animation='wave' height='50px' />
-      <Skeleton animation='wave' height='150px' />
-      <Skeleton animation='wave' height='150px' />
-      <Skeleton animation='wave' height='150px' />
-      <Skeleton animation='wave' height='50px' width='400px' />
+      <Skeleton animation='wave' height='150px' width='320px' />
+      <Box className={classes.flexWrapper}>
+        <Skeleton animation='wave' height='150px' component='div' />
+        <Skeleton animation='wave' height='150px' component='div' />
+        <Skeleton animation='wave' height='150px' component='div' />
+        <Skeleton animation='wave' height='150px' component='div' />
+        <Skeleton animation='wave' height='150px' component='div' />
+        <Skeleton animation='wave' height='150px' component='div' />
+      </Box>
     </>
   );
 };
